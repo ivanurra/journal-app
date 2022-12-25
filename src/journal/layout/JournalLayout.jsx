2b84c58fefb29406fd505f4a36f9ddb7e4e4a642
Ivan Urra <ivan.urra.wd@gmail.com>
2022-12-25 12:13:43 +0100

    WIP

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/system";
+
+const drawerWidth = 240;
+
+export const JournalLayout = ({ children }) => {
+  return (
+    <Box sx={{ display: "flex" }}>
+      {/* Navbar drawerWidth */}
+      {/* Sidebar drawerWidth */}
+
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        {/* Toolbar */}
+        {children}
+      </Box>
+    </Box>
+  );
+};
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,14 @@
+import { JournalLayout } from "../layout/JournalLayout";
 import { Typography } from "@mui/material";
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>
+        Enim anim elit esse ipsum esse fugiat et mollit sint reprehenderit do.
+        Enim labore exercitation aliquip exercitation. Reprehenderit dolor sint
+        ea qui est.
+      </Typography>
+    </JournalLayout>
   );
 };
