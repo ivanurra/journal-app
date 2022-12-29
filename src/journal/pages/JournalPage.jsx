ece670471c0dbc371b0c2edd361e09d700b2cc57
Ivan Urra <ivan.urra.wd@gmail.com>
2022-12-29 10:35:41 +0100

    WIP

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,7 @@
+import { IconButton } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
 import { JournalLayout } from "../layout/JournalLayout";
-import {
-  NoteView,
-  // NothingSelectedView
-} from "../views";
-// import { Typography } from "@mui/material";
+import { NoteView, NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
   return (
@@ -13,8 +11,21 @@ export const JournalPage = () => {
         Enim labore exercitation aliquip exercitation. Reprehenderit dolor sint
         ea qui est.
       </Typography> */}
-      {/* <NothingSelectedView/> */}
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   );
 };
