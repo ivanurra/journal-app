b954b82231e5ec727829e4e96f7e9a5c312ed726
Ivan Urra <ivan.urra.wd@gmail.com>
2022-12-29 09:59:47 +0100

    WIP

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,16 @@
 import { JournalLayout } from "../layout/JournalLayout";
-import { Typography } from "@mui/material";
+import { NothingSelectedView } from "../views";
+// import { Typography } from "@mui/material";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>
+      {/* <Typography>
         Enim anim elit esse ipsum esse fugiat et mollit sint reprehenderit do.
         Enim labore exercitation aliquip exercitation. Reprehenderit dolor sint
         ea qui est.
-      </Typography>
+      </Typography> */}
+      <NothingSelectedView/>
     </JournalLayout>
   );
 };
