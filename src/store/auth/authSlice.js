c44c9bff3983212a12ea0972f54bab1dea5d4e2d
Ivan Urra <ivan.urra.wd@gmail.com>
2023-01-15 14:03:28 +0100

    WIP

---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    status: "checking",
+    uid: null,
+    email: null,
+    displayName: null,
+    photoURL: null,
+    errorMessage: null,
+  },
+  reducers: {
+    login: (state, action) => {},
+    logout: (state, action) => {},
+    checkingCredentials: (state) => {},
+  },
+});
+
+export const { login, logout, checkingCredentials } = authSlice.actions;
