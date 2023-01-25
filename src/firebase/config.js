8fe025589c100c7df59fda0afcd9f0a08d47f7cc
Ivan Urra <ivan.urra.wd@gmail.com>
2023-01-25 20:10:04 +0100

    WIP

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
 
-const key = process.env.REACT_APP_KEY;
-const domain = process.env.REACT_APP_AUTH_DOMAIN;
-const project = process.env.REACT_APP_PROJECTID;
-const bucket = process.env.REACT_APP_STORAGE_BUCKET;
-const message = process.env.REACT_APP_MESSAGING_SENDER_ID;
-const id = process.env.REACT_APP_APP_ID;
+const key = import.meta.env.VITE_APP_KEY;
+const domain = import.meta.env.VITE_APP_AUTH_DOMAIN;
+const project = import.meta.env.VITE_APP_PROJECTID;
+const bucket = import.meta.env.VITE_APP_STORAGE_BUCKET;
+const message = import.meta.env.VITE_APP_MESSAGING_SENDER_ID;
+const id = import.meta.env.VITE_APP_APP_ID;
 
 const firebaseConfig = {
   apiKey: key,
