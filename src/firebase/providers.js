a6cb8ac52d5949e1daf40aa0d15a831d4964091b
Ivan Urra <ivan.urra.wd@gmail.com>
2023-01-26 07:22:14 +0100

    WIP

--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -6,9 +6,22 @@ const googleProvider = new GoogleAuthProvider();
 export const singInWithGoogle = async () => {
   try {
     const result = await signInWithPopup(FirebaseAuth, googleProvider);
-    const credentials = GoogleAuthProvider.credentialFromResult(result);
-    console.log("--->", { credentials });
+    // const credentials = GoogleAuthProvider.credentialFromResult(result);
+    const { displayName, email, photoURL, uid } = result.user;
+    return {
+      ok: true,
+      // User info
+      displayName,
+      email,
+      photoURL,
+      uid,
+    };
   } catch (error) {
-    console.log(error);
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    return {
+      ok: false,
+      errorMessage,
+    };
   }
 };
