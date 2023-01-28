d0acdc11e91a0d3be843c3727a700a83eb7b01f5
Ivan Urra <ivan.urra.wd@gmail.com>
2023-01-28 11:17:19 +0100

    WIP

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -11,8 +11,22 @@ export const authSlice = createSlice({
     errorMessage: null,
   },
   reducers: {
-    login: (state, action) => {},
-    logout: (state, action) => {},
+    login: (state, { payload }) => {
+      state.status = "authenticated";
+      state.uid = payload.uid;
+      state.email = payload.email;
+      state.displayName = payload.displayName;
+      state.photoURL = payload.photoURL;
+      state.errorMessage = null;
+    },
+    logout: (state, { payload }) => {
+      state.status = "no-auth";
+      state.uid = null;
+      state.email = null;
+      state.displayName = null;
+      state.photoURL = null;
+      state.errorMessage = payload.errorMessage;
+    },
     checkingCredentials: (state) => {
       state.status = "checking";
     },
--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,5 +1,5 @@
 import { singInWithGoogle } from "../../firebase/providers";
-import { checkingCredentials } from "./";
+import { checkingCredentials, login, logout } from "./";
 
 export const checkingAuthentication = (email, password) => {
   return async (dispatch) => {
@@ -11,6 +11,7 @@ export const startGoogleSignIn = () => {
   return async (dispatch) => {
     dispatch(checkingCredentials());
     const result = await singInWithGoogle();
-    console.log({ result });
+    if (!result.ok) return dispatch(logout(result.errorMessage));
+    dispatch(login(result));
   };
 };
