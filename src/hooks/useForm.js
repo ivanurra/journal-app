97dafde3fcbf9b4e52749364d427d75a6bf83a77
Ivan Urra <ivan.urra.wd@gmail.com>
2023-01-31 08:01:54 +0100

    WIP

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,7 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const useForm = (initialForm = {}, formValidations = {}) => {
   const [formState, setFormState] = useState(initialForm);
+
+  const [formValidation, setFormValidation] = useState({});
+  useEffect(() => {
+    createValidator();
+  }, [formState]);
 
   const onInputChange = ({ target }) => {
     const { name, value } = target;
@@ -15,10 +20,24 @@ export const useForm = (initialForm = {}, formValidations = {}) => {
     setFormState(initialForm);
   };
 
+  const createValidator = () => {
+    const formCheckedValues = {};
+    for (const formField of Object.keys(formValidations)) {
+      const [fn, errorMessage = "Validation Error"] =
+        formValidations[formField];
+      formCheckedValues[`${formField}Valid`] = fn(formState[formField])
+        ? null
+        : errorMessage;
+    }
+
+    setFormValidation(formCheckedValues);
+  };
+
   return {
     ...formState,
     formState,
     onInputChange,
     onResetForm,
+    ...formValidation,
   };
 };
