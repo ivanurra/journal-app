1fdc9e4597cbe5f6787145ac5b317867f0bbd4cd
Ivan Urra <ivan.urra.wd@gmail.com>
2023-02-09 07:58:07 +0100

    WIP

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -37,6 +37,7 @@ export const RegisterPage = () => {
   const onSubmit = (event) => {
     event.preventDefault();
     setFormSubmitted(true);
+    if (!isFormValid) return;
     console.log(formState);
   };
 
