1c16289cc47e64f98bc7555e61dc64458432ac55
Ivan Urra <ivan.urra.wd@gmail.com>
2023-04-29 17:51:11 +0200

    WIP

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import { useSelector } from "react-redux";
+import { CheckingAuth } from "../ui/";
 
 export const AppRouter = () => {
+  const { status } = useSelector((state) => state.auth);
+
+  if (status === "checking") {
+    return <CheckingAuth />;
+  }
   return (
     <Routes>
       {/* Login and sign up */}
