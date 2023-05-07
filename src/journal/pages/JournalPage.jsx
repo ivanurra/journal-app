0d0a4d72f1657cef9c84ea5da39eabcf83c2bfef
Ivan Urra <ivan.urra.wd@gmail.com>
2023-05-07 12:24:29 +0200

    WIP

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,17 @@
+import { useDispatch } from "react-redux";
 import { IconButton } from "@mui/material";
 import { AddOutlined } from "@mui/icons-material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
+import { startNewNote } from "../../store/journal";
 
 export const JournalPage = () => {
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () => {
+    dispatch(startNewNote());
+  };
+
   return (
     <JournalLayout>
       {/* <Typography>
@@ -14,6 +22,7 @@ export const JournalPage = () => {
       <NothingSelectedView />
       {/* <NoteView /> */}
       <IconButton
+        onClick={onClickNewNote}
         size="large"
         sx={{
           color: "white",
--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,9 +1,18 @@
+import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { FirebaseDB } from "../../firebase/config";
+
 export const startNewNote = () => {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
+    const { uid } = getState().auth;
+
     const newNote = {
       title: "",
       body: "",
       date: new Date().getTime(),
     };
+
+    const newDoc = doc(collection(FirebaseDB, `${uid}/journal/notes`));
+
+    await setDoc(newDoc, newNote);
   };
 };
